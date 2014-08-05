c3967034ffc567a82ec59bdf14d72c5a4098e458
Tony Castronova <castronova.anthony@gmail.com>
2014-08-05 12:50:46 -0600

    Plotting time series when table view is double clicked

--- gui/frmMatPlotLib.py
+++ gui/frmMatPlotLib.py
@@ -1,6 +1,6 @@
 __author__ = 'Mario'
 
-
+from matplotlib.dates import *
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
@@ -13,14 +13,16 @@ import wx
 ] = [wx.NewId() for _init_ctrls in range(4)]
 
 class MatplotFrame(wx.Frame):
-    def __init__(self, parent):
+    def __init__(self, parent, dates, values):
         wx.Frame.__init__(self, parent, id = wx.ID_ANY, title = wx.EmptyString,
-                          pos = wx.DefaultPosition, size = wx.Size( 500,300 ),
+                          pos = wx.DefaultPosition, size = wx.Size( 500,500 ),
                           style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL)
 
         Sizer = wx.BoxSizer(wx.VERTICAL)
 
-        spatialPanel = pnlSpatial(self)
+        spatialPanel = pnlSpatial(self,dates,values)
+
+        #spatialPanel.set_data(dates,values)
 
         Sizer.Add( spatialPanel)
 
@@ -30,22 +32,23 @@ class MatplotFrame(wx.Frame):
 
 class pnlSpatial ( wx.Panel ):
 
-    def __init__( self, prnt):
+    def __init__( self, prnt, x, y):
         wx.Panel.__init__(self, id=wxID_PNLSPATIAL, name=u'pnlIntro', parent=prnt,
               style=wx.TAB_TRAVERSAL)
         self.SetClientSize(wx.Size(10, 10))
 
         self.parent = prnt
-
+        self.x = x
+        self.y = y
 
         # create some sizers
         sizer = wx.BoxSizer(wx.VERTICAL)
 
-        # A button
-        self.button =wx.Button(self, label="Placeholder")
-        self.radiobutton1 = wx.RadioButton(self, wx.ID_ANY, u"Placeholder")
-        self.radiobutton2 = wx.RadioButton(self, wx.ID_ANY, u"Placeholder")
-        self.Bind(wx.EVT_BUTTON, self.OnClick,self.button)
+        # # A button
+        # self.button =wx.Button(self, label="Placeholder")
+        # self.radiobutton1 = wx.RadioButton(self, wx.ID_ANY, u"Placeholder")
+        # self.radiobutton2 = wx.RadioButton(self, wx.ID_ANY, u"Placeholder")
+        # self.Bind(wx.EVT_BUTTON, self.OnClick,self.button)
 
         # put up a figure
         self.figure = plt.figure()
@@ -53,19 +56,38 @@ class pnlSpatial ( wx.Panel ):
         self.canvas = FigureCanvas(self, -1, self.figure)
 
         sizer.Add(self.canvas, 100, wx.ALIGN_CENTER|wx.ALL)
-        sizer.Add(self.button, 0, wx.ALIGN_CENTER|wx.ALL)
-        sizer.Add(self.radiobutton1, 0, wx.ALIGN_CENTER|wx.ALL)
-        sizer.Add(self.radiobutton2, 0, wx.ALIGN_CENTER|wx.ALL)
+        # sizer.Add(self.button, 0, wx.ALIGN_CENTER|wx.ALL)
+        # sizer.Add(self.radiobutton1, 0, wx.ALIGN_CENTER|wx.ALL)
+        # sizer.Add(self.radiobutton2, 0, wx.ALIGN_CENTER|wx.ALL)
 
         self.SetSizer(sizer)
         #self.Fit()
+
     def log(self, fmt, *args):
         print (fmt % args)
+
     def OnClick(self,event):
         self.log("button clicked, id#%d\n", event.GetId())
+
+    # def set_data(self, x,y):
+    #     self._x = x
+    #     self._y = y
+
     def drawplot(self, fig):
+
+
+        dates = date2num(self.x)
+
+
         ax = fig.add_subplot(1,1,1)
-        t = np.arange(0,1,0.001)
-        ax.plot(t,t*t)
+
+        #t = np.arange(0,1,0.001)
+        ax.plot_date(dates, self.y)
+
+        #ax.plot(t,t*t)
         ax.grid()
+
+        fig.autofmt_xdate()
+
         return ax
+

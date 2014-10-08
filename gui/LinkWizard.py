e08b6852474a329f8a9119015647b242cb18befd
Mario Harper <marioharper182@gmail.com>
2014-10-08 11:17:51 -0600

    Quick changes, temporary reverting.

--- gui/LinkWizard.py
+++ gui/LinkWizard.py
@@ -244,11 +244,13 @@ class wizLink(wx.wizard.Wizard):
 
         # Set the initial order of the pages
         self.page1.SetNext(self.page2)
-        self.page2.SetNext(self.page4)
+        self.page2.SetNext(self.page3)
+        self.page3.SetNext(self.page4)
 
        #
         self.page2.SetPrev(self.page1)
-        self.page4.SetPrev(self.page2)
+        self.page3.SetPrev(self.page2)
+        self.page4.SetPrev(self.page3)
         # self.page4.SetNext(self.page5)
         #
         # self.page5.SetPrev(self.page4)
--- gui/pnlTemporal.py
+++ gui/pnlTemporal.py
@@ -19,18 +19,212 @@ class pnlTemporal ( wx.Panel ):
 
         bSizer6 = wx.BoxSizer( wx.VERTICAL )
 
-        m_radioBox1Choices = [ u"PlaceHolder", "PlaceHolder",  "PlaceHolder"]
-        self.m_radioBox1 = wx.RadioBox( self, wx.ID_ANY, u"Temporal Shift", wx.DefaultPosition, wx.DefaultSize, m_radioBox1Choices, 1, wx.RA_SPECIFY_COLS )
-        self.m_radioBox1.SetSelection( 0 )
-        bSizer6.Add( self.m_radioBox1, 0, wx.ALL, 5 )
+    def __init__( self, prnt):
+        wx.Panel.__init__(self, id=wxID_PNLSPATIAL, name=u'pnlIntro', parent=prnt,
+              pos=wx.Point(571, 262), size=wx.Size(10, 10),
+              style=wx.TAB_TRAVERSAL)
+        self.SetClientSize(wx.Size(10, 10))
 
-        m_radioBox2Choices = [ u"PlaceHolder", "PlaceHolder", "PlaceHolder", "PlaceHolder", "PlaceHolder" ]
-        self.m_radioBox2 = wx.RadioBox( self, wx.ID_ANY, u"Time Range", wx.DefaultPosition, wx.DefaultSize, m_radioBox2Choices, 1, wx.RA_SPECIFY_COLS )
-        self.m_radioBox2.SetSelection( 0 )
-        bSizer6.Add( self.m_radioBox2, 0, wx.ALL, 5 )
+        self.parent = prnt
 
-        self.m_radioBtn1 = wx.RadioButton( self, wx.ID_ANY, u"PlaceHolder", wx.DefaultPosition, wx.DefaultSize, 0 )
-        bSizer6.Add( self.m_radioBtn1, 0, wx.ALL, 5 )
+        self.__input_geoms = {}
+        self.__output_geoms = {}
+
+        # create some sizers
+        sizer = wx.BoxSizer(wx.VERTICAL)
+
+
+        # A button
+        # self.button =wx.Button(self, label="Placeholder")
+        # self.radiobutton1 = wx.RadioButton(self, wx.ID_ANY, u"Placeholder")
+        # self.radiobutton2 = wx.RadioButton(self, wx.ID_ANY, u"Placeholder")
+        # self.Bind(wx.EVT_BUTTON, self.OnClick,self.button)
+
+        self.__input_data = []
+        self.__output_data = []
+
+        # self.inputCheckbox = wx.CheckBox(self, wx.ID_ANY,'Inputs')
+        # self.outputCheckbox = wx.CheckBox(self, wx.ID_ANY,'Outputs')
+
+        self.inputCombo = wx.ComboBox(self, wx.ID_ANY,name='input_combo',choices=[])
+        self.outputCombo = wx.ComboBox(self, wx.ID_ANY,name='output_combo', choices=[])
+
+        self.inputLabel = wx.StaticText(self,wx.ID_ANY,label='Input Features: ')
+        self.outputLabel = wx.StaticText(self,wx.ID_ANY,label='Output Features: ')
+#        __init__(self, parent, id=-1, label=EmptyString, pos=DefaultPosition, size=DefaultSize, style=0, name=StaticTextNameStr)
+
+
+        # self.inputCheckbox.Disable()
+        # self.outputCheckbox.Disable()
+
+        # self.inputCheckbox.Bind(wx.EVT_CHECKBOX, self.UpdatePlot)
+        # self.outputCheckbox.Bind(wx.EVT_CHECKBOX, self.UpdatePlot)
+
+        self.inputCombo.Bind(wx.EVT_COMBOBOX, self.UpdatePlot)
+        self.outputCombo.Bind(wx.EVT_COMBOBOX, self.UpdatePlot)
+
+        # put up a figure
+        self.figure = plt.figure()
+        self.ax = self.figure.add_subplot(1,1,1)
+        # self.mapping = self.figure.add_subplot(1,3,2)
+        # self.output = self.figure.add_subplot(1,3,3)
+
+        # format plot axis (suppress)
+
+        self.ax.xaxis._visible = False
+        self.ax.yaxis._visible = False
+        # self.mapping.xaxis._visible = False
+        # self.mapping.yaxis._visible = False
+        # self.output.xaxis._visible = False
+        # self.output.yaxis._visible = False
+
+
+
+        #self.axes = self.drawplot(self.figure)
+        self.canvas = FigureCanvas(self, -1, self.figure)
+
+        sizer.Add(self.canvas, 100, wx.ALIGN_CENTER|wx.ALL)
+        #sizer.Add(self.button, 0, wx.ALIGN_CENTER|wx.ALL)
+        #sizer.Add(self.inputCheckbox, 0, wx.ALIGN_CENTER|wx.ALL)
+        #sizer.Add(self.outputCheckbox, 0, wx.ALIGN_CENTER|wx.ALL)
+
+        # add inputs controls to an iosizer
+        iosizer = wx.BoxSizer(wx.HORIZONTAL)
+        iosizer.Add(self.inputLabel, 1, wx.ALIGN_LEFT|wx.ALL)
+        iosizer.Add(self.inputCombo, 1, wx.ALIGN_LEFT|wx.ALL)
+        sizer.Add(iosizer)
+
+        iosizer = wx.BoxSizer(wx.HORIZONTAL)
+        iosizer.Add(self.outputLabel, 1, wx.ALIGN_LEFT|wx.ALL)
+        iosizer.Add(self.outputCombo, 1, wx.ALIGN_LEFT|wx.ALL)
+        sizer.Add(iosizer)
+        #sizer.Add(self.outputCombo, 0, wx.ALIGN_LEFT|wx.ALL)
+
+        self.SetSizer(sizer)
+        #self.Fit()
+
+
+
+    def log(self, fmt, *args):
+        print (fmt % args)
+
+    def OnClick(self,event):
+        self.log("button clicked, id#%d\n", event.GetId())
+
+    def set_input_data(self, value):
+        """
+        :param value: dictionary {variable: [geoms]}
+        :return:
+        """
+        self.inputCombo.SetItems([' ']+value.keys())
+        self.__input_data = value
+
+    def get_input_geom(self, var_name):
+
+        return self.__input_data[var_name]
+
+    def set_output_data(self, value):
+        """
+        :param value: dictionary {variable: [geoms]}
+        :return:
+        """
+        self.outputCombo.SetItems([' ']+value.keys())
+        self.__output_data = value
+
+    def get_output_geom(self, var_name):
+
+        return self.__output_data[var_name]
+
+    def buildGradientColor(self, num, cmap='Blues'):
+        # get the color map
+        c = getattr(plt.cm, cmap)
+
+        # add two so that the median color is chosen if only one geometry
+        num += 2
+
+        # generate the color definitions
+        colors = [c(1.*i/num) for i in range(0,num)]
+
+        # omit the ends of the spectrum so that the correct number of colors is provided
+        return colors[1:-1]
+
+    def setInputSeries(self):
+
+        inputs = self.input_data()
+        colors = self.buildGradientColor(len(inputs))
+        i = 0
+        for geom in inputs:
+            self.addSeries(geom,colors[i])
+            i += 1
+
+    def setOutputSeries(self):
+
+        outputs = self.output_data()
+        colors = self.buildGradientColor(len(outputs),'jet')
+        i = 0
+        for geom in outputs:
+            self.addSeries(geom,colors[i])
+            i += 1
+
+    def UpdatePlot(self,event):
+
+        self.ax.cla()
+
+        # get parent control
+        parent = event.GetEventObject().Name
+
+        # get variable name
+        var_name = event.GetString()
+
+        # reset the selections
+        self.inputCombo.SetSelection(0)
+        self.outputCombo.SetSelection(0)
+
+        if parent == 'input_combo':
+            data = self.get_input_geom(var_name)
+            if data is not None:
+                colors = self.buildGradientColor(len(data['data']),'Blues')
+                self.SetPlotData(data,colors=colors)
+                self.inputCombo.SetSelection(event.GetSelection())
+            else:
+                self.inputCombo.Disable()
+
+        if parent == 'output_combo':
+            data = self.get_output_geom(var_name)
+            if data is not None:
+                colors = self.buildGradientColor(len(data['data']),'Reds')
+                self.SetPlotData(data,colors=colors)
+                self.outputCombo.SetSelection(event.GetSelection())
+            else:
+                self.outputCombo.Disable()
+
+        # if self.inputCheckbox.IsChecked():
+        #     self.setInputSeries()
+        # if self.outputCheckbox.IsChecked():
+        #     self.setOutputSeries()
+
+        self.canvas.draw()
+
+    def SetPlotData(self, data, colors):
+
+        geoms = data['data']
+        type = data['type']
+        i = 0
+
+        if type == 'Point':
+            tuple_geoms = [g[0] for g in geoms]
+            x,y = zip(*tuple_geoms)
+            self.ax.scatter(x,y,color=colors)
+        else:
+
+            for g in geoms:
+                x,y = zip(*g)
+                self.ax.plot(x,y,color=colors[i])
+                i += 1
+
+        self.ax.grid()
+        self.ax.axis('auto')
+        self.ax.margins(0.1)
 
 
         self.SetSizer( bSizer6 )

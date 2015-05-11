98f130a144376107e78a95b3a89992b6fb9e6377
Tony Castronova <castronova.anthony@gmail.com>
2015-05-11 16:27:04 -0600

    removed unused code in logicEMIT.py [#141]

--- EMIT.py
+++ EMIT.py
@@ -1,49 +1,20 @@
-#!/usr/bin/env python2
-
-
-
 __author__ = 'Mario'
 
-import os
 import sys
 import wx
 import wx.xrc
 import wx.aui
-
 from gui.controller.logicEMIT import LogicEMIT
-import coordinator.engineAccessors as engine
 
-
-# todo: refactor
-# from gui.mainGui import wxStdOut
-
-
-# ##########################################################################
-# # Class MainFrame
-# ##########################################################################
 
 class EMITApp(wx.App):
     def OnInit(self):
-
-        # get the shared instance of the coordinator engine
-        # engine = engineManager.get_engine()
-
-        # connections = engine.getDbConnections()
-
-        # connect to databases and set default
-        #currentdir = os.path.dirname(os.path.abspath(__file__))
-        #connections_txt = os.path.abspath(os.path.join(currentdir,'./data/connections'))
 
         # We are terminating dependency logging errors, We may want this in the future but it
         # tends to add clutter to our console.
         wx.Log.SetLogLevel(0)
 
-        # engine.connect_to_db([connections_txt])
-        # if not engine.get_default_db():
-        #     engine.set_default_database()
-
         self.logicEmit = LogicEMIT(None)
-
 
         return True
 

5e3add85a071ff2293243b688941500edb342b50
Mario Harper <marioharper182@gmail.com>
2015-05-13 15:02:33 -0600

    [#139] The folders should now be in place, we can even set the sim folders as a different icon.

--- gui/controller/logicToolbox.py
+++ gui/controller/logicToolbox.py
@@ -33,9 +33,11 @@ class LogicToolbox(ViewToolbox):
             self.cat = self.tree.AppendItem(self.root, txt)
             if count == 0:
                 self.simCategory = self.tree.AppendItem(self.root, 'Sim Files')
+                self.tree.SetItemImage(self.simCategory, self.fldropenidx, which=wx.TreeItemIcon_Expanded)
+                self.tree.SetItemImage(self.simCategory, self.fldropenidx, which=wx.TreeItemIcon_Normal)
                 count += 1
             self.tree.SetItemImage(self.cat, self.fldropenidx, which=wx.TreeItemIcon_Expanded)
-            # self.tree.SetItemImage(self.cat, self.fldropenidx, which=wx.TreeItemIcon_Normal)
+            self.tree.SetItemImage(self.cat, self.fldropenidx, which=wx.TreeItemIcon_Normal)
             for d in data:
                 path = d['path']
                 apath = join(dirname(abspath(__file__)), '../' + path)

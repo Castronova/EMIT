170edcc67da7499ece0998a6b38a7abedde21fb2
farrieta9 <francisco.arrieta@aggiemail.usu.edu>
2015-06-12 12:01:14 -0600

    Uncommented line 19 from logicEMIT.py.

--- gui/controller/logicEMIT.py
+++ gui/controller/logicEMIT.py
@@ -16,7 +16,7 @@ class LogicEMIT(ViewEMIT):
         # connect to known databases
         currentdir = os.path.dirname(os.path.abspath(__file__))
         connections_txt = os.path.abspath(os.path.join(currentdir, '../../data/connections'))
-        # engine.connectToDbFromFile(dbtextfile=connections_txt)
+        engine.connectToDbFromFile(dbtextfile=connections_txt)
 
         dropTarget = LogicFileDrop(self.Canvas, self.FloatCanvas)
         self.SetDropTarget(dropTarget)

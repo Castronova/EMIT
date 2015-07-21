bf271fc7058c0aaa966fc8d841964b6e39ff042d
Tony Castronova <castronova.anthony@gmail.com>
2015-07-21 11:18:54 -0600

    more modifications to the sqlite building scripts.  Consolidated build_empty and build_populated scripts.  Refactored cvload and load_wof to eliminate the need for subprocess

--- db/scripts/build_populated_db.py
+++ db/scripts/build_populated_db.py
@@ -1,15 +1,24 @@
 import os
-import pyspatialite.dbapi2 as sqlite3
-import subprocess
 import cvload
+import load_wof_to_ODM2
+import pyspatialite.dbapi2 as sqlite3
+
+print 'This script will perform the following tasks: '
+print '1. Create an ODM2 database '
+print '2. Load the ODM2 CV terms '
+print '3. Create a database load script containing no sample data (odm2_empty.sql)'
+print '4. Load sample data into the ODM2 database '
+print '5. Create a database load script containing sample data (odm2_pop.sql) \n\n'
+
+
+# cannot get in-memory, shared cache to work!
 
 # remove db file if it already exists
-print 'Removing existing odm2.sqlite database...',
-dbpath = os.path.abspath('odm2_pop.sqlite')
+print 'Removing existing temp.sqlite database...',
+dbpath = os.path.abspath('temp.sqlite')
 if os.path.exists(dbpath):
     os.remove(dbpath)
 print 'done'
-
 
 # connect to the sqlite database
 conn = sqlite3.connect(dbpath)
@@ -24,10 +33,14 @@ print 'done'
 
 # load controlled vocabularies
 cvload.load_cv("sqlite:///"+dbpath)
-# subprocess.call(["python cvload.py sqlite:///"+dbpath], shell=True)
+
+# create empty database SQL dump file
+with open('odm2_empty.sql', 'w') as f:
+    for line in conn.iterdump():
+        f.write('%s\n' % line)
 
 # load some data
-subprocess.call(["python load_wof_to_ODM2.py"], shell=True)
+load_wof_to_ODM2.load_wof(dbpath)
 
 # add some people
 print 'Adding Person Records...',
@@ -50,8 +63,20 @@ c.executescript(ddl)
 conn.commit()
 print 'done'
 
+# create populated database SQL dump file
+with open('odm2_pop.sql', 'w') as f:
+    for line in conn.iterdump():
+        f.write('%s\n' % line)
+
 # close database connection
 c.close()
 conn.close()
+
+# remove temp sqlite database
+print 'Cleaning...',
+dbpath = os.path.abspath('temp.sqlite')
+if os.path.exists(dbpath):
+    os.remove(dbpath)
+print 'done'
 
 print 'Finished'

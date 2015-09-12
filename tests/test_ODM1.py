ae62bc809b611e00dec721fb336f727f096c2753
farrieta9 <francisco.arrieta@aggiemail.usu.edu>
2015-09-11 19:11:19 -0600

    Added more methods to the test_OMD1.

--- tests/test_ODM1.py
+++ tests/test_ODM1.py
@@ -3,8 +3,6 @@ __author__ = 'francisco'
 from suds.client import Client
 import xml.etree.ElementTree as et
 
-#  Put a break point on the pass to see the values
-
 class ODM1:
 
     def __init__(self):
@@ -30,33 +28,41 @@ class ODM1:
             Make sure to include 'iutah:' in front of the site code
         '''
         conn = self.connectToNetwork(redButteCreek)
-        site = self.getSite(conn, sitecode)
-        self.parseSite(site)
+        site = self.getXMLSite(conn, sitecode)
+        var = self.getXMLVariables(conn)
+        self.getSiteInfo(site)
 
 
     def connectToNetwork(self, link):
         connection = Client(link)
         return connection
 
-    def getSite(self, conn, sitecode):
+    def getXMLSite(self, conn, sitecode):
         # Returns an XML in a array of arrays
         xmlSite = conn.service.GetSites(sitecode)
         return xmlSite
 
-    def parseSite(self, site):
+    def getXMLVariables(self, conn):
+        # Returns an XML in a array of arrays
+        xmlVar = conn.service.GetVariables()
+        return xmlVar
+
+    def createXMLFileForReading(self, site):
+        # Open this file in a browser to view it parsed
+        file = open("test.xml", "w")
+        file.write(site)
+        file.close()
+
+    def getSiteInfo(self, site):
         tree = et.fromstring(site)
-        # queryInfo = tree[0]
-        # site = tree[1]
-        # siteInfo = tree[1][0]
-        # siteCode = tree[1][0][1]
+        siteInfo_Dictionary = {}
 
         for site in tree:
-            print site.tag
             for siteInfo in site:
-                print siteInfo
                 for info in siteInfo:
-                    print info
-        pass
+                    siteInfo_Dictionary[siteInfo.getchildren()[0].text] = siteInfo.getchildren()
+
+        return siteInfo_Dictionary  # The key is the site name
 
 if __name__ == '__main__':
     odm1 = ODM1()

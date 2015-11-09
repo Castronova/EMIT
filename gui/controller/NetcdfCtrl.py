263130ba7e42822af819f892e80627082ab44c92
Ryan Johnson <ryan.z.johnson@oulook.com>
2015-11-09 16:11:10 -0700

    [#267] Added more functionality to the controller. Can get the URL of the item they want. Will work on getting the netcdf item into our system

--- gui/controller/NetcdfCtrl.py
+++ gui/controller/NetcdfCtrl.py
@@ -20,8 +20,23 @@ class NetcdfCtrl(NetcdfViewer):
         self.thredds = "http://www.unidata.ucar.edu/namespaces/thredds/InvCatalog/v1.0"
         self.xlink = "http://www.w3.org/1999/xlink"
         NetcdfViewer.__init__(self, parent=parent)
-
+        self.Bind(wx.EVT_BUTTON, self.addToCanvas, self.add_to_canvas_btn)
         self.Bind(wx.EVT_BUTTON, self.RunCrawler, self.get_btn)
+
+    def addToCanvas(self, event):
+        item = self.getSelectedInformation()
+        # this will get the url we want
+        url = self.TableValues[item][1]
+        print url
+        print "Adding to canvas: SEarch HELLO THIS IS ADDING"
+
+    def getSelectedInformation(self):
+        num = self.variableList.GetItemCount()
+        for i in range(num):
+            if self.variableList.IsSelected(i):
+                v_name = self.variableList.GetItemText(1)
+                return i
+
 
     def get_server_status_code(self, url):
         """
@@ -62,11 +77,11 @@ class NetcdfCtrl(NetcdfViewer):
     def RunCrawler(self, event):
         results = [];
         url = self.url_textbox.GetLineText(0)
-        isValid = self.check_url(url)
+        isValid = self.check_url(url + "/catalog.xml")
 
         if isValid:
             results = self.crawler(url +"/catalog.xml",results)
-            TableValues = [];
+            self.TableValues = [];
             spacing = 25
             print '\n\nFilename',(spacing-len('Filename'))*' ','DAP Path'
             print '--------',(spacing-len('Filename'))*' ','--------'
@@ -83,12 +98,12 @@ class NetcdfCtrl(NetcdfViewer):
                     wms_url = dict(wms.items())['urlPath']
                     name = dict(ds.items())['name']
 
-                    TableValues.append([name, url + dap_url, url + dap_url + ".das"])
+                    self.TableValues.append([name, url + dap_url, url + dap_url + ".das"])
                     print name,(spacing-len(name))*' ',dap_url
 
-            for x in TableValues:
+            for x in self.TableValues:
                 print x
-            self.updateFileList(TableValues)
+            self.updateFileList(self.TableValues)
 
 
     def crawler(self,catalog, results):

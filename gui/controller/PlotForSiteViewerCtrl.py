3ac08f72af59aad24aa7c8c9d2430dfd922d8b85
farrieta9 <francisco.arrieta@aggiemail.usu.edu>
2016-01-12 18:49:53 -0700

    #322 Fixed some sites not plotting
    If all the values were not a number than it failed so i made it check if it was and if values are not a number than don't plot.

--- gui/controller/PlotForSiteViewerCtrl.py
+++ gui/controller/PlotForSiteViewerCtrl.py
@@ -7,6 +7,22 @@ class logicPlotForSiteViewer(ViewPlotForSiteViewer):
     def __init__(self, panel):
         ViewPlotForSiteViewer.__init__(self, panel)
 
+    def clearPlot(self):
+        self.axes.clear()
+        self.axes.grid()
+        self.axes.margins(0)
+        self.reDraw()
+
+    def isNan(self, values):  # if values is not a number than return true
+        count = 0
+        for i in range(len(values)):
+            if numpy.isnan(values[i]):
+                count += 1
+        if count == len(values):
+            return True
+        else:
+            return False
+
     def plotData(self, data, name, noDataValue):
 
         if len(data) == 0:
@@ -16,6 +32,9 @@ class logicPlotForSiteViewer(ViewPlotForSiteViewer):
         dates, values = zip(*data)
         nvals = numpy.array(values, dtype=numpy.float)
         nvals[nvals == noDataValue] = None
+        if self.isNan(nvals):
+            print "nvals are not a number"
+            return
 
         # plot datetime axis
         self.axes.plot_date(x=dates, y=nvals, label=name, linestyle='-', marker=None)
@@ -24,10 +43,4 @@ class logicPlotForSiteViewer(ViewPlotForSiteViewer):
 
     def reDraw(self):
         self.plot.draw()
-
-    def clearPlot(self):
-        self.axes.clear()
-        self.axes.grid()
-        self.axes.margins(0)
-        self.reDraw()
 

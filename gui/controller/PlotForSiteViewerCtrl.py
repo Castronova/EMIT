9e97c11d65dcd4c43c9ef5d0fe740af08ae4ffa0
farrieta9 <francisco.arrieta@aggiemail.usu.edu>
2016-01-12 17:58:20 -0700

    #322 Fixed wof not opening
    Also fixed some of the sites not working.  There are still some that are getting a different error

--- gui/controller/PlotForSiteViewerCtrl.py
+++ gui/controller/PlotForSiteViewerCtrl.py
@@ -6,19 +6,8 @@ import numpy
 class logicPlotForSiteViewer(ViewPlotForSiteViewer):
     def __init__(self, panel):
         ViewPlotForSiteViewer.__init__(self, panel)
-        self.displayLegend(0)
 
-        # stores the plot objects
-        self.plots = []
-        self.__plot_count = 0
-
-        # stores the axis objects
-        self.__axis = []
-
-        # matplotlib color cycle used to ensure primary and secondary axis are not displayed with the same color
-        self.__color_cycle = self.axes._get_lines.color_cycle
-
-    def plotData(self, data, name, noDataValue, ylabel):
+    def plotData(self, data, name, noDataValue):
 
         if len(data) == 0:
             return
@@ -28,56 +17,17 @@ class logicPlotForSiteViewer(ViewPlotForSiteViewer):
         nvals = numpy.array(values, dtype=numpy.float)
         nvals[nvals == noDataValue] = None
 
-        # get the next line color
-        color = self.getNextColor()
-
-
-        if self.__plot_count == 0:
-            # plot data on the primary axis
-            p = self.axes.plot_date(dates, nvals, label=name, color=color, linestyle='-', marker=None)
-            self.axes.legend(p, [pl.get_label() for pl in self.plots], loc=0)
-            self.axes.set_ylabel(ylabel)
-
-        elif self.__plot_count > 0:
-            # plot data on the secondary axis
-            ax = self.axes.twinx()
-            self.__axis.append(ax)
-            p = ax.plot_date(dates, nvals, label=name, color=color, linestyle='-', marker=None)
-            ax.set_ylabel(ylabel)
-
-        # save each of the plots
-        self.plots.extend(p)
-
-        # rebuild the legend
-        self.axes.legend(self.plots, [pl.get_label() for pl in self.plots], loc=0)
-
-        # increment the plot counter
-        self.__plot_count += 1
-
-        # redraw the cavas
+        # plot datetime axis
+        self.axes.plot_date(x=dates, y=nvals, label=name, linestyle='-', marker=None)
+        self.displayLegend(0)
         self.reDraw()
 
     def reDraw(self):
         self.plot.draw()
 
     def clearPlot(self):
-
-        # clear axis
         self.axes.clear()
-        for ax in self.__axis:
-            ax.cla()
-
-        # reset the axis container
-        self.__axis = []
-
         self.axes.grid()
         self.axes.margins(0)
-
-        # clear the plot objects
-        self.__plot_count = 0
-        self.plots = []
-
         self.reDraw()
 
-    def getNextColor(self):
-         return next(self.__color_cycle)

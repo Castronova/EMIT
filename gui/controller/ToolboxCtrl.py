d6de69f2089ae69b29be1fa0af8bf11b65a552c0
farrieta9 <francisco.arrieta@aggiemail.usu.edu>
2016-01-16 12:43:37 -0700

    #258 Sample models are in the installer
    Organized the models in app_data into directories, and and modified the way the toolbox reads the path of models a bit.  Issue now is that models are not adding to canvas in the installer.

--- gui/controller/ToolboxCtrl.py
+++ gui/controller/ToolboxCtrl.py
@@ -13,6 +13,7 @@ import ConfigParser
 import fnmatch
 from coordinator.emitLogging import elog
 from environment import env_vars
+from sprint import *
 # todo: refactor
 from gui.views.ModelView import ViewModel
 
@@ -88,7 +89,12 @@ class LogicToolbox(ViewToolbox):
                 # populate models
                 if 'Components' in folder_path:
                     for p in path.split(';'):
-                        apath = join(dirname(abspath(__file__)), '../../' + p)
+                        # apath = join(dirname(abspath(__file__)), '../../' + p)
+                        # sPrint(apath)
+                        # sPrint("Realpath: " + str(os.path.realpath(__file__)))
+                        # sPrint("Try this path: " + str(os.path.realpath(__file__ + "../../../../" + p)))
+                        # sPrint("Better????: " + str(os.path.realpath(__file__ + "../../../../app_data/samples")))
+                        apath = os.path.realpath(__file__ + "../../../../" + p)
                         matches = []
                         self.dirlist = []
 

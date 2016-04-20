91286cca026e943d87ca394c06b3b2af5e90226c
castronova <castronova.anthony@gmail.com>
2016-04-20 11:55:12 -0600

    [#405] fixes to the windows development environment build script

--- dev_environment/build-dev-env-windows.bat
+++ dev_environment/build-dev-env-windows.bat
@@ -16,19 +16,52 @@ exit /B
 
 :: Remove the existing environment
 echo Removing the existing environment 
-conda env remove --yes -n temp
+conda env remove --yes -n %1
 
 :: Create a new environment
 echo Creating a fresh environment 
-conda env create -n temp -f emit-env-win python=2.7 
+conda env create -n %1 -f %2 python=2.7 
+
+set dashed="-------------------------------------------------------------"
+
 
 :: activate the environment
-activate temp &&^
+activate %1 &&^
+echo %dashed% &&^
+echo Installing GeoAlchemy &&^
+echo %dashed% &&^
+pip install  git+https://github.com/ODM2/geoalchemy.git@v0.7.3#egg=geoalchemy-0.7.3 &&^
+echo %dashed% &&^
+echo Installing odm2api &&^
+echo %dashed% &&^
+pip install  git+https://github.com/ODM2/ODM2PythonAPI.git &&^
+echo %dashed% &&^
 echo Installing APSW &&^
+echo %dashed% &&^
 pip install whl\apsw-3.11.1.post1-cp27-cp27m-win_amd64.whl &&^
+echo %dashed% &&^
 echo Installing Shapely &&^
+echo %dashed% &&^
 pip install whl\Shapely-1.5.7-cp27-none-win_amd64.whl &&^
-echo Installing ODM2PythonAPI &&^
-pip install --process-dependency-links git+https://github.com/ODM2/ODM2PythonAPI.git &&^
+echo %dashed% &&^
+echo Installing GDAL &&^
+echo %dashed% &&^
+pip install whl\GDAL-1.11.2-cp27-none-win_amd64.whl &&^
+echo %dashed% &&^
 echo Fresh Development Environment Created &&^ 
+echo %dashed% &&^
 deactivate &&^
+
+:: notes
+:: conda create -n temp python=2.7
+:: conda install pandas sqlalchemy networkx wxpython pyshp suds psycopg2 netcdf4 matplotlib seaborn pil requests geoalchemy2 
+:: pip install  git+https://github.com/ODM2/geoalchemy.git@v0.7.3#egg=geoalchemy-0.7.3
+:: pip install  git+https://github.com/ODM2/ODM2PythonAPI.git
+:: pip install whl\Shapely-1.5.7-cp27-none-win_amd64.whl
+:: pip install whl\GDAL-1.11.2-cp27-none-win_amd64.whl
+:: pip install whl\apsw-3.11.1.post1-cp27-cp27m-win_amd64.whl
+
+
+
+
+

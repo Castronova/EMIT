80bb2ecf22b52841fb7b0241188d6c67d829d1c1
farrieta9 <francisco.arrieta@aggiemail.usu.edu>
2016-04-28 15:28:53 -0600

    #433 Cleaning and refactored EMITCtrl.py
    Moved the path to the files to environment.py

--- gui/controller/EMITCtrl.py
+++ gui/controller/EMITCtrl.py
@@ -1,35 +1,22 @@
-import os
 import sqlite3 as lite
-import sys
 from gui.controller.UserCtrl import UserCtrl
-import wx
-
+import environment
 import coordinator.engineAccessors as engine
 from gui.views.EMITView import ViewEMIT
-from utilities.gui import loadAccounts
 from sprint import *
 
-# todo: the connections file should be a binary object (pickle)
 
 class EMITCtrl(ViewEMIT):
     def __init__(self, parent):
 
-
         ViewEMIT.__init__(self, parent)
         self.FloatCanvas = self.Canvas.FloatCanvas
 
+        connections_txt = environment.getDefaultConnectionsTextPath()
+        script_path = environment.getDefaultScriptPath()
+        filepath = environment.getDefaultLocalDBPath()
+
         # connect to known databases
-        currentdir = os.path.dirname(os.path.abspath(__file__))
-        connections_txt = os.path.abspath(os.path.join(currentdir, '../../data/connections'))
-        script_path = os.path.abspath(os.path.join(currentdir, "../../app_data/db/.dbload"))
-        filepath = os.path.abspath(os.path.join(currentdir, "../../app_data/db/local.db"))
-
-        # change file paths if app is running as installed package
-        if getattr(sys, 'frozen', False):
-            connections_txt = os.path.join(sys._MEIPASS, 'data/connections')
-            filepath = os.path.join(sys._MEIPASS, 'app_data/db/local.db')
-            script_path = os.path.join(sys._MEIPASS, "app_data/db/.dbload")
-
         # if the database is not found in the dir (dev mode) or app (install mode), create it
         if not os.path.exists(filepath):
             conn = lite.connect(filepath)
@@ -38,7 +25,6 @@ class EMITCtrl(ViewEMIT):
                 cur = conn.cursor()
                 cur.executescript(script.read())
             script.close()
-
 
         # connect to databases defined in the connections file
         engine.connectToDbFromFile(dbtextfile=connections_txt)
@@ -53,9 +39,3 @@ class EMITCtrl(ViewEMIT):
             controller = UserCtrl(self)
             controller.CenterOnScreen()
             controller.Show()
-
-def removedb(file):
-    try:
-        os.remove(file)
-    except OSError:
-        pass

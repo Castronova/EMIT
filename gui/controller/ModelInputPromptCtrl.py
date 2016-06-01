d6996e071f9ec219eaf2c7aba0ecf23a80c2678c
farrieta9 <francisco.arrieta@aggiemail.usu.edu>
2016-06-01 15:34:25 -0600

    Implemented the browse button to open the file dialog

--- gui/controller/ModelInputPromptCtrl.py
+++ gui/controller/ModelInputPromptCtrl.py
@@ -6,7 +6,12 @@ class ModelInputPromptCtrl(ModelInputPromptView):
     def __init__(self, parent, path):
         ModelInputPromptView.__init__(self, parent, path)
 
+        # Bindings
         self.Bind(wx.EVT_CLOSE, self.on_close)
+        for button in self.inputs:
+            if button:
+                button.Bind(wx.EVT_BUTTON, self.on_file_browser)
+
 
     ####################################
     # EVENTS
@@ -15,3 +20,8 @@ class ModelInputPromptCtrl(ModelInputPromptView):
     def on_close(self, event):
         self.MakeModal(False)
         self.Destroy()
+
+    def on_file_browser(self, event):
+        file_browser = wx.FileDialog(self, message="Load file")
+        if file_browser.ShowModal() == wx.ID_OK:
+            self.text_ctrls[event.GetId()].SetValue(file_browser.GetPath())

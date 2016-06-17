537cef004e69e16f222f7e605e3f1093877cc8fd
Tony Castronova <castronova.anthony@gmail.com>
2016-06-17 11:28:08 -0600

    updated the weather readed to use the new wof output format.  It also builds exchange items from this csv instead of relying on the mdl file "

--- app_data/models/weather/weather.py
+++ app_data/models/weather/weather.py
@@ -1,56 +1,105 @@
-__author__ = 'tonycastronova'
-
 from wrappers import feed_forward
 import stdlib
-from utilities import mdl
+from utilities import mdl, geometry
 from datetime import datetime as dt
+import numpy
+import uuid
 
 class weatherReader(feed_forward.Wrapper):
     def __init__(self, config_params):
         super(weatherReader, self).__init__(config_params)
 
-        # build inputs and outputs
-        io = mdl.build_exchange_items_from_config(config_params)
-        self.outputs(value=io[stdlib.ExchangeItemType.OUTPUT])
-
-        # set inputs and outputs
-        self.p = self.outputs()['Precipitation']
-
         # get weather data file
-        self.weather_data_path = config_params['model inputs'][0]['weather_csv']
+        self.weather_data_path = config_params['weather_csv']
+
+        # object to hold weather data
+        self.weather_data = {}
+
+        # build oeis
+        self.construct_oeis(self.weather_data_path)
+
+        # set the model's status
+        self.status(stdlib.Status.READY)
 
 
     def run(self, inputs):
 
-        # read weather data
-        incremental_precip = []
-        dates = []
-        with open(self.weather_data_path, 'rU') as f:
-            lines = f.readlines()
+        oeis = self.outputs()
 
-            # skip commented lines
-            skip = 0
-            for line in lines:
-                if line[0] == '#':
-                    skip += 1
-                else:
-                    # exit loop as soon as non-commented line is found
-                    break
+        # loop through all of the output exchange items and set their output data
+        for name, oei in oeis.items():
 
-            # read all lines after header
-            for line in lines[skip:]:
-                data = line.split(',')
+            # get the dates and values that were parsed during initialization
+            dates, values = self.weather_data[oei.id()]
 
-                # exit if the data is empty
-                if data[0].strip() == '':
-                    break
+            # set output data
+            oei.setValues2(values, dates)
 
-                # save dates to list
-                dates.append(dt.strptime(data[0], "%m/%d/%y %H:%M"))
+        return 1
 
-                # save incremental precipitation to list
-                incremental_precip.append(float(data[2]))
+    def finish(self):
+        self.status(stdlib.Status.FINISHED)
+
+    def construct_oeis(self, weather_data):
 
 
-        # set output data
-        self.p.setValues2(incremental_precip, dates)
+        # read the weather data csv
+        f = open(weather_data, 'r')
+        lines = f.readlines()
+        f.close()
+
+        # read ei metadata
+        ei_data = []
+        for i in range(16,len(lines)):
+            line = lines[i]
+            if line[0] == '#' and len(line) > 3:
+                if line[2] == 'V' and line[3] != '[':
+                    data = line.split('=')
+                    data = data[1].split(',')
+                    trimmed = [d.strip() for d in data]
+                    ei_data.append(trimmed)
+            elif line[0] != '#':
+                break
+
+        def make_date(datestr):
+            return dt.strptime(datestr, '%m-%d-%Y %H:%M:%S')
+
+        # parse the weather data dates and values into numpy arrays
+        date_arr = numpy.genfromtxt(weather_data, delimiter=',', converters={'Date':make_date}, names = ['Date'], dtype=None, usecols=[0])
+        val_arr = numpy.genfromtxt(weather_data, delimiter=',', dtype=float)
+        val_arr = numpy.delete(val_arr,0,1)
+
+        # build exchange items
+        col_idx = 0
+        for item in ei_data:
+
+            # map to stdlib units and variables
+            unit = mdl.create_unit(item[2])
+            variable = mdl.create_variable(item[1])
+
+            uid = uuid.uuid4().hex
+
+            ei = stdlib.ExchangeItem(id=uid,
+                                     name=item[0],
+                                     unit=unit,
+                                     variable=variable,
+                                     desc=item[3],
+                                     type=stdlib.ExchangeItemType.OUTPUT)
+
+            # build geometry
+            pt = geometry.build_point_geometry(float(item[-2]), float(item[-1]))
+            ei.addGeometry(pt)
+
+            # add the oei to the outputs list
+            self.outputs(value=ei)
+
+            # save the data associated with this exchange item
+            self.weather_data[uid] = [date_arr, val_arr[:, col_idx]]
+
+            # increment the column index for the numpy array
+            col_idx += 1
+
+
+
+
+
